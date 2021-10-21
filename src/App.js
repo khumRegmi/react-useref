9c767e95c06f0c08a3f1c38a261a221506400c77
Khum Regmi <kpr.regmi088@gmail.com>
2021-10-21 18:27:43 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+
+import "./App.css";
+
+// FIRST USE OF "useref" //
+
+// function App() {
+//   const [name, setName] = useState("");
+//   const count = useRef(1);
+//   const onChangeHandler = (e) => {
+//     setName(e.target.value);
+//   };
+
+//   useEffect(() => {
+//     count.current = count.current + 1;
+//   });
+
+//   return (
+//     <div className="App">
+//       <input value={name} placeholder="name" onChange={onChangeHandler}></input>
+//       <p>My name is {name}</p>
+//       <p>The component is rendered {count.current} times</p>
+//     </div>
+//   );
+// }
+
+// SECOND USE OF "useref" ,change focus
+
+// function App() {
+//   const [name, setName] = useState("");
+//   const [display, setDisplay] = useState("");
+//   const inputRef = useRef();
+
+//   const focus = () => {
+//     inputRef.current.focus();
+//   };
+
+//   const submit = () => {
+//     setDisplay(`${name}`);
+//   };
+
+//   return (
+//     <div>
+//       <input
+//         ref={inputRef}
+//         placeholder="name"
+//         onChange={(e) => setName(e.target.value)}
+//       ></input>
+//       <p>My name is {display}</p>
+//       <button onClick={focus}>Focus</button>
+//       <button onClick={submit}>Submit</button>
+//     </div>
+//   );
+// }
+
+// THIRD USE: store previous value
 
 function App() {
+  const [name, setName] = useState("");
+  const inputRef = useRef("");
+
+  useEffect(() => {
+    inputRef.current = name;
+  }, [name]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        placeholder="name"
+        onChange={(e) => setName(e.target.value)}
+      ></input>
+      <p>
+        My name is {name} and it used to be {inputRef.current}
+      </p>
     </div>
   );
 }
